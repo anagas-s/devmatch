2e719dd4a485474cc68ea5d498f35c970e32f240
Anagas <anagassatheeshkumar@gmail.com>
2024-10-27 21:41:04 +0530

    Create user feed api and implement pagination

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const { userAuth } = require("../middleware/auth");
 const connectionRequest = require("../models/connectionRequest");
+const User = require("../models/user");
 const userRouter = express.Router();
 
 // Get all the pending connection request for the logged in user
@@ -15,6 +16,75 @@ userRouter.get("/user/requests/received", userAuth, async (req, res) => {
       .populate("fromUserId", ["firstName", "lastName"]);
 
     res.json({ message: "Received requests", data: connectionRequests });
+  } catch (err) {
+    res.status(400).send({ error: err.message });
+  }
+});
+
+userRouter.get("/user/requests/connection", userAuth, async (req, res) => {
+  try {
+    const loggedInUser = req.user;
+    const connections = await connectionRequest
+      .find({
+        $or: [{ fromUserId: loggedInUser._id }, { toUserId: loggedInUser._id }],
+        status: "accepted",
+      })
+      .populate("fromUserId", ["firstName", "lastName"])
+      .populate("toUserId", ["firstName", "lastName"]);
+
+    const data = connections.map((row) => {
+      if (row.fromUserId._id.equals(loggedInUser._id)) {
+        return row.toUserId;
+      }
+      return row.fromUserId;
+    });
+
+    res.json({ message: "Connections", data: data });
+  } catch (err) {
+    res.status(400).send({ error: err.message });
+  }
+});
+
+userRouter.get("/feed", userAuth, async (req, res) => {
+  // User should see all the user cards except
+  // 0. his own card
+  // 1. his connections
+  // 2. ignored People
+  // 3. already sent the connection request
+
+  try {
+    const loggedInUser = req.user;
+
+    const page = parseInt(req.query.page) || 1;
+    let limit = parseInt(req.query.limit) || 10;
+    limit = limit > 50 ? 50 : limit;
+
+    const skip = (page - 1) * limit;
+
+    //Find all connection request (sent+received);
+    const connections = await connectionRequest
+      .find({
+        $or: [{ fromUserId: loggedInUser._id }, { toUserId: loggedInUser._id }],
+      })
+      .select("fromUserId  toUserId");
+
+    const hideUsersFromFeed = new Set();
+
+    connections.forEach((req) => {
+      hideUsersFromFeed.add(req.fromUserId.toString());
+      hideUsersFromFeed.add(req.toUserId.toString());
+    });
+    const users = await User.find({
+      $and: [
+        { _id: { $nin: Array.from(hideUsersFromFeed) } },
+        { _id: { $ne: loggedInUser._id } },
+      ],
+    })
+      .select("firstName lastName about skills")
+      .skip(skip)
+      .limit(limit);
+
+    res.send(users);
   } catch (err) {
     res.status(400).send({ error: err.message });
   }
